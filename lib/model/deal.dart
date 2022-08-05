1b8030ac69216fa9db47f7f48f00b4d218259666
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-08-05 10:34:38 +0800

    Add files via upload

---
+++ lib/model/deal.dart
@@ -0,0 +1,25 @@
+class DealData {
+  final String? imageURL;
+  final String? businessName;
+  final String? category;
+  final String? address;
+  final String? description;
+  final String? tagline;
+  final int? hourOpen;
+  final int? hourClose;
+  final int? totalOffers;
+  final int? totalUsed;
+
+  DealData({
+    this.imageURL,
+    this.businessName,
+    this.category,
+    this.address,
+    this.description,
+    this.tagline,
+    this.hourOpen,
+    this.hourClose,
+    this.totalOffers,
+    this.totalUsed,
+  });
+}
