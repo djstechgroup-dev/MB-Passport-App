919827e3c58f7354287863feb40ef8bafa96099c
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-09-29 08:21:07 +0800

    Add files via upload

---
+++ lib/account_tabs/notifications.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:passportapp/notification_screen.dart';
+
+class AccountNotification extends StatelessWidget {
+  const AccountNotification({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NotificationScreen();
+  }
+}
