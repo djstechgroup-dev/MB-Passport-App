49815c1d97cc4cfcfea7aa6decad78e72bb46779
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-10-01 15:27:16 +0800

    Add files via upload

---
+++ lib/services/deal_service.dart
@@ -0,0 +1,131 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:passportapp/model/deal.dart';
+
+class DealApi {
+  Future<List<dynamic>> getAllDeals(String? token) async {
+    try {
+      //Temporary 10.0.2.2:8000 for testing using emulator
+      final uri = Uri.http('10.0.2.2:8000', '/api/deals');
+      final headers = {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      };
+      var response = await http.get(
+        uri,
+        headers: headers,
+      );
+
+      final output = jsonDecode(response.body);
+      if(response.statusCode == 200) {
+        // print(output['deals'][0]);
+        // print(output['deals'][0]['locations'][0]['address']);
+        // print(output['deals'][0]['businessId']['businessName']);
+        List<dynamic> deals = output['deals'];
+        // print(deals[0]['_id']);
+        // print(deals[2]['_id']);
+        return deals;
+      } else {
+        throw Exception("ERROR WITH STATUS: ${response.statusCode}");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  Future<DealData> getDealById(String? token, String? id) async {
+    try {
+      //Temporary 10.0.2.2:8000 for testing using emulator
+      final uri = Uri.http('10.0.2.2:8000', '/api/deal/$id');
+      final headers = {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      };
+      var response = await http.get(
+        uri,
+        headers: headers,
+      );
+
+      final output = jsonDecode(response.body);
+      if(response.statusCode == 200) {
+        // print(output['deal']);
+        return DealData.fromJson(output);
+      } else {
+        throw Exception("ERROR WITH STATUS: ${response.statusCode}");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  Future<String> getDealOfTheDay(String? token) async {
+    try {
+      //Temporary 10.0.2.2:8000 for testing using emulator
+      final uri = Uri.http('10.0.2.2:8000', '/api/dotd');
+      final headers = {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      };
+      var response = await http.get(
+        uri,
+        headers: headers,
+      );
+
+      final output = jsonDecode(response.body);
+      if(response.statusCode == 200) {
+        return output['deal'];
+      } else {
+        throw Exception("ERROR WITH STATUS: ${response.statusCode}");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  void saveDeal(String? token, String? id) async {
+    try {
+      //Temporary 10.0.2.2:8000 for testing using emulator
+      final uri = Uri.http('10.0.2.2:8000', '/api/save-deal/$id');
+      final headers = {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      };
+      var response = await http.patch(
+        uri,
+        headers: headers,
+      );
+
+      if(response.statusCode == 200) {
+        print("Successfully added!");
+      } else {
+        throw Exception("ERROR WITH STATUS: ${response.statusCode}");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  void unSaveDeal(String? token, String? id) async {
+    try {
+      //Temporary 10.0.2.2:8000 for testing using emulator
+      final uri = Uri.http('10.0.2.2:8000', '/api/remove-save-deal/$id');
+      final headers = {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      };
+
+      var response = await http.patch(
+        uri,
+        headers: headers,
+      );
+
+      if(response.statusCode == 200) {
+        print("Successfully removed!");
+      } else {
+        throw Exception("ERROR WITH STATUS: ${response.statusCode}");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:passportapp/attributes.dart';
+import 'package:passportapp/main_screen.dart';
+import 'package:passportapp/model/user.dart';
+import 'package:passportapp/model/weather.dart';
+import 'package:passportapp/onboard_screen.dart';
+import 'package:passportapp/services/auth_service.dart';
+import 'package:passportapp/services/location_service.dart';
+import 'package:passportapp/services/weather_service.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  WeatherService weatherService = WeatherService();
+  Weather weather = const Weather();
+
+  @override
+  void initState() {
+    super.initState();
+    checkIfUserAvailable();
+  }
+
+  Future<void> getAttributesUser() async {
+    setState(() {
+    //   UserAttributes.favoriteBusiness = snap['favoriteBusiness'];
+    //   UserAttributes.savedDeals = snap['savedDeals'];
+    //   UserAttributes.offersRedeemed = snap['offersRedeemed'];
+    //   UserAttributes.savingsEarned = snap['savingsEarned'];
+      UserAttributes.profilePicURL = FirebaseAuth.instance.currentUser?.photoURL;
+    });
+    _navigateNext(const MainScreen());
+  }
+
+  Future<void> getWeather() async {
+    weather = await weatherService.getData("33.699966477945296,-78.89252252946339");
+    setState(() {
+      Attributes.currentTemp = weather.temperature;
+      Attributes.currentUV = weather.uvi;
+      Attributes.currentPrecipitation = weather.rain * 10;
+    });
+    //Then get attribute user
+    getAttributesUser();
+  }
+
+  void getCurrentLocationAndWeather() async {
+    LocationService().getLatLong().then((value) {
+      setState(() {
+        Attributes.locationData = value!;
+      });
+      //Get weather after lat long
+      getWeather();
+    });
+  }
+
+  void checkIfUserAvailable() {
+    AuthService().checkUserAvailable().then((value) async {
+      if(value == true) {
+        //Get current loc and weather first
+        getCurrentLocationAndWeather();
+      } else {
+        //Directly to onboarding
+        _navigateNext(const OnBoardingScreen());
+      }
+    });
+  }
+
+  Future _navigateNext(Widget child) async {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+            builder: (context) => child,
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/images/logo.png",
+          width: screenWidth / 2,
+        ),
+      ),
+    );
+  }
+}
