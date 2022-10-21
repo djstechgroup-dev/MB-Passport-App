9b3c6102f5a6f09ccd8fe934bfacbe7041e97913
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-10-21 22:41:34 +0800

    Delete android directory

--- android/app/src/main/kotlin/com/localiti/mbpassport/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.localiti.mbpassport
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/myrtle/passportapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.myrtle.passportapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
